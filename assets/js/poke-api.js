94d5a846ca12a1cc4cb7d04d4ebb89799ded3427
Cleyton Albuquerque <cleyton.albuquerque@dcx.ufpb.br>
2023-12-13 16:40:31 -0300

    Commit initial

---
+++ assets/js/poke-api.js
@@ -0,0 +1,11 @@
+const pokeApi = {}
+
+pokeApi.getPokemons = function () {
+    const offset = 0
+    const limit = 10
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+     return fetch(url)
+    .then(response => response.json())
+    .then(responseBody => responseBody.results)
+    .catch ((error) => console.log(error))
+}
